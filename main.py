5f05bc4737c7ee181e24e3b30a58e57063923092
bueckerl <bueckerl@uni-paderborn.de>
2023-12-27 14:12:32 +0100

    Implemented format conversion to mp3 and destination picking

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import tkinter as tk
 import customtkinter
 from downloader import Downloader
+import os
 
 # System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
+
+destinationPath = os.getcwd()
+defaultPath = True
+print("Destination (DEFAULT): " + destinationPath)
 
 def create_link_list():
     text = link.get("0.0", "end")
     linkList = text.splitlines()
     print(linkList)
 
+    fileNameList = []
+
     for url in linkList:
-        Downloader.downloadMP3(url);
+        fileNameList.append(Downloader.downloadMP3(url, destinationPath))
+
+    print(fileNameList)
+
+    # Convert files to mp3
+    for file in fileNameList:
+        file = str(file)
+        fileName, ext = file.split(sep=".")
+
+        if defaultPath:
+            source = destinationPath + "\\" + fileName + ".mp4"
+            dest = destinationPath + "\\" + fileName + ".mp3"
+        else:
+            source = destinationPath + "/" + fileName + ".mp4"
+            dest = destinationPath + "/" + fileName + ".mp3"
+
+        print("Src: " + source + " Dest: " + dest)
+
+        Downloader.convertToMP3(source, dest)
+        os.remove(source)
+
+
+def open_file_dialog():
+    foldername = tk.filedialog.askdirectory()
+    global destinationPath 
+    destinationPath = foldername
+    global defaultPath
+    defaultPath = False
+    print("Destination: " + foldername)  # prints the selected folder path
 
 if __name__ == '__main__':
     
@@ -32,5 +67,9 @@ if __name__ == '__main__':
     # Download Button
     download_button = customtkinter.CTkButton(app, text="Download", command=create_link_list)
     download_button.pack(padx=10, pady=10)    
+
+    # File Dialog Button
+    file_dialog_button = customtkinter.CTkButton(app, text="Zielverzeichnis auswählen", command=open_file_dialog)
+    file_dialog_button.pack(padx=10, pady=10)
     
     app.mainloop()
