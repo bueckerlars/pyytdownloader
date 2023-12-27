03c820b428de31561f3d8afea14ff95b2f4bafb2
bueckerl <bueckerl@uni-paderborn.de>
2023-12-27 23:05:34 +0100

    CleanUp and removed downloader class

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import tkinter as tk
 import customtkinter
-from downloader import Downloader
 import os
+from pytube import YouTube
+from moviepy.editor import *
+import string
 
 # System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-destinationPath = os.getcwd()
+destinationPath = os.getcwd() + "\\"
 defaultPath = True
 print("Destination (DEFAULT): " + destinationPath)
+
+def downloadMP3(url: string, dest: string) -> string:
+        try:
+            ytObject = YouTube(url)
+            audio = ytObject.streams.get_highest_resolution()
+            audio.download(output_path=dest)
+
+            print("Download successfull | url: " + url)  
+            return audio.default_filename
+        except:
+            print("Download failed | url: " + url)      
+
+def convertToMP3(mp4, mp3):
+    FILETOCONVERT = AudioFileClip(mp4)
+    FILETOCONVERT.write_audiofile(mp3)
+    FILETOCONVERT.close()
 
 def create_link_list():
     text = link.get("0.0", "end")
@@ -19,7 +37,7 @@ def create_link_list():
     fileNameList = []
 
     for url in linkList:
-        fileNameList.append(Downloader.downloadMP3(url, destinationPath))
+        fileNameList.append(downloadMP3(url, destinationPath))
 
     print(fileNameList)
 
@@ -28,31 +46,27 @@ def create_link_list():
         file = str(file)
         fileName, ext = file.split(sep=".")
 
-        if defaultPath:
-            source = destinationPath + "\\" + fileName + ".mp4"
-            dest = destinationPath + "\\" + fileName + ".mp3"
-        else:
-            source = destinationPath + "/" + fileName + ".mp4"
-            dest = destinationPath + "/" + fileName + ".mp3"
-
+        source = destinationPath + fileName + ".mp4"
+        dest = destinationPath + fileName + ".mp3"
         print("Src: " + source + " Dest: " + dest)
 
-        Downloader.convertToMP3(source, dest)
+        convertToMP3(source, dest)
         os.remove(source)
 
 
 def open_file_dialog():
-    foldername = tk.filedialog.askdirectory()
+    foldername = tk.filedialog.askdirectory() + "/"
     global destinationPath 
     destinationPath = foldername
     global defaultPath
     defaultPath = False
+    destLabel.configure(text=destinationPath)
     print("Destination: " + foldername)  # prints the selected folder path
 
 if __name__ == '__main__':
-    
     # App Frame
     app = customtkinter.CTk()
+    app.resizable(height=False, width=False)
     app.geometry("720x480")
     app.title("YouTube Downloader")
     
@@ -68,8 +82,16 @@ if __name__ == '__main__':
     download_button = customtkinter.CTkButton(app, text="Download", command=create_link_list)
     download_button.pack(padx=10, pady=10)    
 
+    destLabel = customtkinter.CTkLabel(app, text=destinationPath)
+    destLabel.pack()
+
     # File Dialog Button
     file_dialog_button = customtkinter.CTkButton(app, text="Zielverzeichnis auswählen", command=open_file_dialog)
     file_dialog_button.pack(padx=10, pady=10)
+
+    # Progress Bar
+    stepProgressBar = customtkinter.CTkProgressBar(app, width=400)
+    stepProgressBar.set(0)
+    stepProgressBar.pack(padx=10, pady=10)
     
     app.mainloop()
